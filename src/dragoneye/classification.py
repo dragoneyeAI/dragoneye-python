624d353b3c7c8f5d755f33c92ad3fc1ce3116824
dragoneye-alex <130794170+dragoneye-alex@users.noreply.github.com>
2024-07-18 08:05:42 -0400

    Add support for product predictions

--- src/dragoneye/classification.py
+++ src/dragoneye/classification.py
@@ -5,7 +5,7 @@ import requests
 from pydantic import BaseModel
 
 from .types.common import BASE_API_URL, NormalizedBbox, TaxonID, TaxonPrediction
-from .types.image import Image
+from .types.image import Image, assert_consistent_data_type
 
 if TYPE_CHECKING:
     from .client import Dragoneye
@@ -28,6 +28,15 @@ class ClassificationPredictImageResponse(BaseModel):
     predictions: Sequence[ClassificationObjectPrediction]
 
 
+class ClassificationProductPrediction(BaseModel):
+    category: TaxonPrediction
+    traits: Sequence[ClassificationTraitRootPrediction]
+
+
+class ClassificationPredictProductResponse(BaseModel):
+    predictions: Sequence[ClassificationProductPrediction]
+
+
 class Classification:
     def __init__(self, client: "Dragoneye"):
         self._client = client
@@ -43,9 +52,7 @@ class Classification:
         if image.file_or_bytes is not None:
             if isinstance(image.file_or_bytes, bytes):
                 files["image_file"] = io.BytesIO(image.file_or_bytes)
-            elif isinstance(
-                image.file_or_bytes, BinaryIO
-            ):  # pyright: ignore [reportUnnecessaryIsInstance]
+            elif isinstance(image.file_or_bytes, BinaryIO):  # pyright: ignore [reportUnnecessaryIsInstance]
                 files["image_file"] = image.file_or_bytes
             else:
                 raise ValueError("Invalid image type: Must be bytes or BinaryIO")
@@ -69,3 +76,42 @@ class Classification:
             )
 
         return ClassificationPredictImageResponse.model_validate(response.json())
+
+    def predict_product(
+        self, images: Sequence[Image], model_name: str
+    ) -> ClassificationPredictProductResponse:
+        url = f"{BASE_API_URL}/predict-product"
+
+        files = []
+        data = {}
+
+        assert_consistent_data_type(images)
+
+        for image in images:
+            if image.file_or_bytes is not None:
+                if isinstance(image.file_or_bytes, bytes):
+                    files.append(("image_file", io.BytesIO(image.file_or_bytes)))
+                elif (
+                    isinstance(image.file_or_bytes, BinaryIO)  # pyright: ignore [reportUnnecessaryIsInstance]
+                    or issubclass(type(image.file_or_bytes), BinaryIO)
+                    or isinstance(image.file_or_bytes, io.BufferedReader)
+                ):
+                    files.append(("image_file", image.file_or_bytes))
+                else:
+                    raise ValueError("Invalid image type: Must be bytes or BinaryIO")
+            elif image.url is not None:
+                data.setdefault("image_urls", []).append(image.url)
+
+        data["model_name"] = model_name
+
+        headers = {"Authorization": f"Bearer {self._client.api_key}"}
+
+        try:
+            response = requests.post(url, files=files, data=data, headers=headers)
+            response.raise_for_status()
+        except requests.RequestException as error:
+            raise Exception(
+                "Error during Dragoneye Classification prediction request:", error
+            )
+
+        return ClassificationPredictProductResponse.model_validate(response.json())
